ccde252d91a28c5e11713d6ab2d085d83e9f370c
Marco Polichetti <gremo1982@gmail.com>
2022-05-18 09:12:08 +0200

    Update syncbackfree to version 10.2.28.0 closes #2

--- syncbackfree/tools/chocolateyinstall.ps1
+++ syncbackfree/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   url            = 'https://www.2brightsparks.com/assets/software/SyncBack_Setup.exe'
-  checksum       = '1def0700b7f3c1c745cc9002916e1fb55cdf2247db5f4ebd9ce505a8704016d6'
+  checksum       = '386d6e50ed2ad3392f3e8b8ec2ac4d87263012add01118cb8e94263bbe97bfb8'
   checksumType   = 'sha256'
   silentArgs     = "/NORESTART /SP- /SUPPRESSMSGBOXES /VERYSILENT"
   validExitCodes = @(0, 3010, 1641)
