720d531d9dc2582790d3c646d7153a81ee33f4a7
Marco Polichetti <gremo1982@gmail.com>
2023-01-25 13:14:47 +0100

    Update runjs to version 2.7.5

--- runjs/tools/chocolateyinstall.ps1
+++ runjs/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
-  url            = 'https://github.com/lukehaas/RunJS/releases/download/v2.6.0/RunJS-Setup-2.6.0.exe'
-  checksum       = 'fcbd528a51946e57ae0a0e354686b84f4d767baa98c94b2ddc88d93a38e544da'
+  url            = 'https://github.com/lukehaas/RunJS/releases/download/v2.7.5/RunJS-Setup-2.7.5.exe'
+  checksum       = 'efe9457eece36e1da07d4c72e518a5cc50a46fc35beb53aecdef37737e89cbb2'
   checksumType   = 'sha256'
   silentArgs     = "/S"
   validExitCodes = @(0)
